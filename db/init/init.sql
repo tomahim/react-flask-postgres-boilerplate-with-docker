831f87a4584bd2181f2414caa61c15713bb96b76
tomabolt <thimblot@gmail.com>
2018-09-02 16:02:48 +0200

    refactor bash script

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,5 +1,10 @@
+PRINT N'CREATE TABLE players ------------------------------------------- mlkmlkmlk ';
+
 CREATE TABLE players (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(100) NOT NULL,
     lastname VARCHAR(100) NOT NULL
 );
+
+
+INSERT INTO players VALUES ("test", "test2");
--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,46 +1,23 @@
 #!/bin/bash
 
+declare -A arr
 
-BACKEND_PATH="${PWD}/backend"
-BACKEND_NAME="sharefolder-backend"
+arr["sharefolder-backend"]="${PWD}/backend"
+arr["sharefolder-client"]="${PWD}/client"
+arr["sharefolder-db-init"]="${PWD}/db/init"
+arr["sharefolder-db-conf"]="${PWD}/db/conf"
 
-CLIENT_PATH="${PWD}/client"
-CLIENT_NAME="sharefolder-client"
-
-DB_PATH="${PWD}/db/init"
-DB_NAME="sharefolder-db"
-
-sharedfolder_exists=`vboxmanage showvminfo default | grep "Name: '${BACKEND_NAME}'"`
-if [[ -z $sharedfolder_exists ]]
-then
-    vboxmanage sharedfolder add default --name ${BACKEND_NAME} --hostpath ${BACKEND_PATH} --transient
-else
-    echo "[INFO] ${BACKEND_NAME} already set"
-fi
-
-sharedfolder_exists=`vboxmanage showvminfo default | grep "Name: '${CLIENT_NAME}'"`
-if [[ -z $sharedfolder_exists ]]
-then
-    vboxmanage sharedfolder add default --name ${CLIENT_NAME} --hostpath ${CLIENT_PATH} --transient
-else
-    echo "[INFO] ${CLIENT_NAME} already set"
-fi
-
-sharedfolder_exists=`vboxmanage showvminfo default | grep "Name: '${DB_NAME}'"`
-if [[ -z $sharedfolder_exists ]]
-then
-    vboxmanage sharedfolder add default --name ${DB_NAME} --hostpath ${DB_PATH} --transient
-else
-    echo "[INFO] ${DB_NAME} already set"
-fi
-
-docker-machine ssh default "sudo mkdir -p ${BACKEND_PATH}"
-docker-machine ssh default "sudo mount -t vboxsf -o defaults,uid=`id -u`,gid=`id -g` ${BACKEND_NAME} ${BACKEND_PATH}"
-
-docker-machine ssh default "sudo mkdir -p ${CLIENT_PATH}"
-docker-machine ssh default "sudo mount -t vboxsf -o defaults,uid=`id -u`,gid=`id -g` ${CLIENT_NAME} ${CLIENT_PATH}"
-
-docker-machine ssh default "sudo mkdir -p ${DB_PATH}"
-docker-machine ssh default "sudo mount -t vboxsf -o defaults,uid=`id -u`,gid=`id -g` ${DB_NAME} ${DB_PATH}"
+for key in ${!arr[@]}; do
+    echo ${key} ${arr[${key}]}
+    sharedfolder_exists=`vboxmanage showvminfo default | grep "Name: '${key}'"`
+    if [[ -z $sharedfolder_exists ]]
+    then
+        vboxmanage sharedfolder add default --name ${key} --hostpath ${arr[${key}]} --transient
+    else
+        echo "[INFO] ${key} already set"
+    fi
+    docker-machine ssh default "sudo mkdir -p ${arr[${key}]}"
+    docker-machine ssh default "sudo mount -t vboxsf -o defaults,uid=`id -u`,gid=`id -g` ${key} ${arr[${key}]}"
+done
 
 docker-compose up --build
